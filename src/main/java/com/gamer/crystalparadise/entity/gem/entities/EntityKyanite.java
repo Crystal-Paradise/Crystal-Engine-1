8617cdee655d822d86e6140a4b55b5d480a824c9
gamer <jupiterinc3@gmail.com>
2023-07-06 01:44:07 +0100

    Add Kyanite

---
+++ src/main/java/com/gamer/crystalparadise/entity/gem/entities/EntityKyanite.java
@@ -0,0 +1,203 @@
+package com.gamer.crystalparadise.entity.gem.entities;
+
+import com.gempire.entities.ai.*;
+import com.gempire.entities.bases.EntityGem;
+import com.gempire.entities.other.EntityAbomination;
+import com.gempire.entities.other.EntityCrawler;
+import com.gempire.entities.other.EntityShambler;
+import com.gempire.util.Abilities;
+import com.gempire.util.GemPlacements;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+
+public class EntityKyanite extends EntityGem {
+    public EntityKyanite(EntityType<? extends PathfinderMob> type, Level worldIn) {
+        super(type, worldIn);
+    }
+
+    public static AttributeSupplier.Builder registerAttributes() {
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 40.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.4D)
+                .add(Attributes.ATTACK_DAMAGE, 3.0D)
+                .add(Attributes.ATTACK_SPEED, 1.0D);
+    }
+
+    @Override
+    public Float baseXScale() {
+        return .9F;
+    }
+
+    @Override
+    public Float baseYScale() {
+        return 1.0F;
+    }
+
+    @Override
+    public Float baseZScale() {
+        return .9F;
+    }
+
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(7, new FloatGoal(this));
+        this.goalSelector.addGoal(6, new PanicGoal(this, 1.1D));
+        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 4.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(7, new EntityAIWander(this, 1.0D));
+        this.goalSelector.addGoal(7, new EntityAIFollowAssigned(this, 1.0D));
+        this.goalSelector.addGoal(7, new EntityAIFollowOwner(this, 1.0D));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityAbomination.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityShambler.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityCrawler.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Mob.class, 1, false, false, (p_234199_0_) -> p_234199_0_.getClassification(true) == MobCategory.MONSTER));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.1D, false));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityGem.class, 1, false, false, this::checkRebel));
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGemGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGemGoal(this));
+        this.goalSelector.addGoal(1, new EntityAISludged(this, 0.6));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityGem.class, 1, false, false, this::checkBothSludged));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 1, false, false, this::checkSludged));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, EntityGem.class, 6.0F, 1.0D, 1.2D, this::checkElseSludged));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, EntityCrawler.class, 6.0F, 1.0D, 1.2D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, EntityShambler.class, 6.0F, 1.0D, 1.2D));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, EntityAbomination.class, 6.0F, 1.0D, 1.2D));
+    }
+    @Override
+    public SoundEvent getInstrument()
+    {
+        return SoundEvents.NOTE_BLOCK_BASS;
+    }
+
+    @Override
+    public int generateSkinVariant() {
+        return this.getGemPlacement() == 11 ? 11 : 0;
+    }
+
+    @Override
+    public GemPlacements[] getPlacements() {
+        return new GemPlacements[] {
+                GemPlacements.FOREHEAD, GemPlacements.LEFT_EYE, GemPlacements.RIGHT_EYE, GemPlacements.NOSE, GemPlacements.MOUTH, GemPlacements.LEFT_CHEEK, GemPlacements.RIGHT_CHEEK, GemPlacements.CHEST, GemPlacements.BACK, GemPlacements.BELLY,
+                GemPlacements.LEFT_SHOULDER, GemPlacements.RIGHT_SHOULDER, GemPlacements.LEFT_ARM, GemPlacements.RIGHT_ARM, GemPlacements.LEFT_HAND, GemPlacements.RIGHT_HAND, GemPlacements.LEFT_PALM, GemPlacements.RIGHT_PALM, GemPlacements.LEFT_THIGH, GemPlacements.RIGHT_THIGH,
+                GemPlacements.LEFT_KNEE, GemPlacements.RIGHT_KNEE, GemPlacements.LEFT_ANKLE, GemPlacements.RIGHT_ANKLE };
+    }
+
+    @Override
+    public int generateHairVariant() {
+        return this.random.nextInt(3);
+    }
+
+    @Override
+    public int generateInsigniaColor() {
+        return this.random.nextInt(16);
+    }
+
+    @Override
+    public int generateOutfitColor() {
+        return this.random.nextInt(16);
+    }
+
+    @Override
+    public boolean hasOutfitPlacementVariant() {
+        return true;
+    }
+
+    @Override
+    public int[] outfitPlacementVariants() {
+        return new int[] { 17 };
+    }
+
+    public Abilities[] possibleAbilities(){
+        return new Abilities[]{
+                Abilities.NO_ABILITY, Abilities.TANK, Abilities.BEEFCAKE, Abilities.POWERHOUSE, Abilities.UNHINGED
+        };
+    }
+    public Abilities[] definiteAbilities(){
+        return new Abilities[]{
+                Abilities.REFINERY, Abilities.TINKERER
+        };
+    }
+
+    public int generateSkinColorVariant() {
+        return 0;
+    }
+
+    @Override
+    public boolean generateIsEmotional() {
+        return true;
+    }
+
+    @Override
+    public byte EmotionThreshold() {
+        return 10;
+    }
+
+    public boolean canChangeUniformColorByDefault() {
+        return true;
+    }
+
+    public boolean canChangeInsigniaColorByDefault(){
+        return true;
+    }
+
+    @Override
+    public boolean fireImmune(){
+        return true;
+    }
+
+    public boolean hasSkinColorVariant(){
+        return false;
+    }
+
+    public int generateOutfitVariant() {
+        if (getGemPlacement() == 17)
+            return 17;
+        return this.random.nextInt(4);
+    }
+
+    public int generateInsigniaVariant(){
+        return this.getOutfitVariant();
+    }
+
+    public int generateRebelInsigniaVariant() {
+        return this.getRebelOutfitVariant();
+    }
+
+    @Override
+    public int generateVisorVariant() {
+        return this.random.nextInt(4);
+    }
+
+    @Override
+    public int baseFocus() {
+        return 7;
+    }
+
+    public int generateHardness() {
+        return 2;
+    }
+
+    @Override
+    public int getColor() {
+        return 2;
+    }
+
+    @Override
+    public int exitHoleSize() {
+        return 3;
+    }
+
+    @Override
+    public ModelPart getArm(HumanoidArm p_102852_) {
+        return p_102852_ == HumanoidArm.LEFT ? this.arml : this.armr;
+    }
+}
